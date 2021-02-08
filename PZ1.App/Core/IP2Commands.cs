c0c6bca12bf22fd6d5831669e7299d918bc9f0d3
olelit <litasov00@gmail.com>
2021-02-08 22:32:52 +0300

    Добавить русскоязычный вывод в проект

---
+++ PZ1.App/Core/IP2Commands.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PZ1.App.Core
+{
+    class IP2Commands
+    {
+        public IP2Commands()
+        {
+
+        }
+
+        public void Arp()
+        {
+
+        }
+        public void Ipconfig()
+        {
+            SendCommand("ipconfig /all");
+        }
+        public void Getmac()
+        {
+
+        }
+        public void Nbtsat()
+        {
+
+        }
+        public void Netsh()
+        {
+
+        }
+        public void Net()
+        {
+
+        }
+        public void Ping()
+        {
+
+        }
+        public void Route()
+        {
+
+        }
+        public void SendCommand(string command)
+        {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                StandardOutputEncoding = Encoding.GetEncoding(866),
+                FileName = "cmd.exe",
+                Arguments = "/C " + command,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+            process.Start();
+            MessageBox.Show(process.StandardOutput.ReadToEnd());
+            //_Output.Text = ;
+
+            process.WaitForExit();
+        }
+    }
+}
---
+++ PZ1.App/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using PZ1.App.Core;
+using System.Text;
+using System.Windows;
+
+namespace PZ1.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly IP2Commands _Command;
+
+        public MainWindow()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            InitializeComponent();
+            _Command = new IP2Commands();
+        }
+
+        private void Ipconfig_Click(object sender, RoutedEventArgs e)
+        {
+            _Command.Ipconfig();
+        }
+    }
+}
